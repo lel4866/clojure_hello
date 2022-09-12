883f2ec877e10eaebaafd24ffb62878f02cda94e
Lawrence Lewis <lel486@gmail.com>
2022-09-12 08:39:26 -0400

    implement mypline to rplace non-working (inside Vim) println

--- src/clojure_hello/core.clj
+++ src/clojure_hello/core.clj
@@ -2,18 +2,27 @@
   (:require [java-time :as jt])
   (:gen-class))
 
+(defn mypline [mystr]
+  "replacement for println since println doest display in gvim on Windows"
+  (print (str mystr "\n"))) 
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (def a (conj args "grt"))
-  (println a)
-  a)
+  (mypline a)
+  ;; (println "hello")
+  (+ 2 3))
 
 (-main "abc" "def")
 
-(+ 3 54)
+(+ 3 54 37)
 
 (jt/local-date)
 (jt/local-time)
 (jt/local-date-time)
+
+(+ 2 3)
+
+(mypline [1 2 3])
 
